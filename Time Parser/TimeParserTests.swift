b6a6c863158fbe11d19044bf29437b718f6485aa
tidalboot <tidalboot@googlemail.com>
2015-07-07 16:39:13 +0100

    Added tests

---
+++ Time Parser/TimeParserTests.swift
@@ -0,0 +1,40 @@
+//
+//  TimeParserTests.swift
+//  
+//
+//  Created by Nick Jones on 07/07/2015.
+//
+//
+
+import Foundation
+import TimeParser
+import XCTest
+
+
+class TimeParserTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        var myDate = NSDate()
+        myDate.ti
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
